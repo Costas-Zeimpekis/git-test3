2842958802b10cc23733179dd2fc421e26a97d16
Georgios Doudouliakis <dudugeorge89@gmail.com>
2020-12-14 16:34:14 +0200

    Show first 3 posts of user with id = 1

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ async function getPosts() {
   ).then((response) => response.json());
 
   const postsUser1 = posts.filter((post) => post.userId === 1);
+  const first3posts = posts.slice(0, 3);
+  console.log(first3posts);
 
-  postsUser1.forEach((post) => {
+  first3posts.forEach((post) => {
     appContainer.innerHTML += `
             <div class="post">
                 <h1>${post.title}</h1>
