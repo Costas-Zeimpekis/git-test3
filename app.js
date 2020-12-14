d52928614b37f90b64e2c92006b6298f9ba20b4d
Georgios Doudouliakis <dudugeorge89@gmail.com>
2020-12-14 15:34:28 +0200

    Fetch posts and log them

---
+++ app.js
@@ -0,0 +1,5 @@
+async function getPosts() {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
