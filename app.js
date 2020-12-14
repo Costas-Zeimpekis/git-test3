8166b9105448e15210bd06bb58b03eec1d3b67e3
Georgios Doudouliakis <dudugeorge89@gmail.com>
2020-12-14 16:43:02 +0200

    Chain array methods

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ async function getPosts() {
     "https://jsonplaceholder.typicode.com/posts"
   ).then((response) => response.json());
 
-  const postsUser1 = posts.filter((post) => post.userId === 1);
-  const first3posts = postsUser1.slice(0, 3);
-  console.log(first3posts);
+  //   const postsUser1 = posts.filter((post) => post.userId === 1);
+  //   const first3posts = postsUser1.slice(0, 3);
+  //   console.log(first3posts);
 
-  first3posts.forEach((post) => {
-    appContainer.innerHTML += `
+  posts
+    .filter((post) => post.userId === 1)
+    .slice(0, 3)
+    .forEach((post) => {
+      appContainer.innerHTML += `
             <div class="post">
                 <h1>${post.title}</h1>
             </div>
         `;
-  });
+    });
 }
 
 getPosts();
